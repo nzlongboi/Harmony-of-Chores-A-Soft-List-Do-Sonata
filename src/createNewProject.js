39c99bb5eb738a9d0a17f7bb6c4f19fcaeabf072
nzlongboi <116671677+nzlongboi@users.noreply.github.com>
2024-02-11 15:46:23 +1300

    Logic for creating a project. Using forms to input information

---
+++ src/createNewProject.js
@@ -0,0 +1,57 @@
+
+
+
+class Project {
+    constructor() {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.counter = counter;
+    }
+
+    createProject() {
+        // creating a form element
+        const projectForm = document.createElement('form');
+        projectForm.classList.add('project-form');
+
+        // creating the input elements for the form
+        const projectNameInput = document.createElement('input');
+        projectNameInput.setAttribute('type', 'text');
+        projectNameInput.setAttribute('placeholder', 'Enter project name');
+        projectNameInput.setAttribute('required', 'true');
+
+        // add description, due date, priority and counter
+
+        // creating the submit button
+        const submitButton = document.createElement('button');
+        submitButton.setAttribute('type', 'submit');
+        submitButton.innerText = 'Create Project';
+
+        projectForm.addEventListener('submit', (event) => {
+            event.preventDefault();
+
+            // retrive the project name from the input field
+            const projectName = projectNameInput.value;
+
+            // create project DOM element and append it to the projects page
+            const projectElement = document.createElement('div');
+            projectElement.innerText = `Project: ${projectName}`;
+
+            const projectsPage = document.getElementById('content');
+            projectsPage.appendChild(projectElement);
+
+            projectForm.reset();
+        });
+
+        projectForm.appendChild(projectNameInput);
+        projectForm.appendChild(submitButton);
+    }
+
+}
+
+
+
+
+
+export default Project;
